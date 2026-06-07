60544eff86d41f08f9ed38c1edca628ed87273a3
Milos Subotic <milos.subotic.sm@gmail.com>
2026-06-07 14:05:18 +0200

    Install script.

---
+++ install_for_dev_on_Ubuntu.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $SCRIPT_DIR
+
+sudo apt -y install \
+    python3-virtualenv \
+    imagemagick \
+    python3-pip \
+    ipython3
+
+PREFIX=$HOME/.local/opt/labelme/
+mkdir -p $PREFIX
+virtualenv $PREFIX
+
+source $PREFIX/bin/activate
+
+pip install --upgrade pip
+
+# install in editable mode
+pip install -e .
+
+cat > ~/.local/bin/labelme_yolo << EOF
+#!/bin/bash
+source ~/.local/opt/labelme/bin/activate
+labelme \$@
+exit \$?
+EOF
+chmod a+x ~/.local/bin/labelme_yolo
+
+
+
